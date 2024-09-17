71771be0004d37eebb137df442b5b39c73c8013a
Mohammed <145200425+MohammedJunz@users.noreply.github.com>
2024-09-17 21:02:21 +0600

    Add files via upload

---
+++ Techproverse/index.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector(".side")
+    sidebar.style.display = "flex"
+}
+function hideSidebar(){
+    const sidebar = document.querySelector(".side")
+    sidebar.style.display = "none"
+}
